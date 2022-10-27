c487456e80a94cca0a20a8d922b7542a879be184
Viet Nguyen <viet.nguyen2@hitachivantara.com>
2022-10-27 19:49:45 +0700

    #20 Tạo tables, thêm dữ liệu database với Sequelize CLI - Cấu hình database cho Node.JS phần 2

---
+++ server/src/server.js
@@ -0,0 +1,25 @@
+// import express from "express";
+const express = require("express");
+// import bodyParser from "body-parser";
+const bodyParser = require("body-parser");
+// import viewEngine from "./config/viewEngine";
+const viewEngine = require("./config/viewEngine");
+// import initWebRoutes from "./route/web";
+const initWebRoutes = require("./route/web");
+
+require("dotenv").config();
+
+const app = express();
+
+// config app
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+viewEngine(app);
+initWebRoutes(app);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log("Backend Node JS is Running on the port: ", port);
+});
