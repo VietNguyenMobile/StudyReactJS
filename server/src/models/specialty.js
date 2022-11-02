f6f121072faf8a39db9b34166b9da77f42032ee8
VIET-NGUYEN\Administrator <quocviet.ce@gmail.com>
2022-11-02 15:35:50 +0700

    #24 Tạo Tất Cả Models, Migrations và Seeders Cho Dự Án Đặt Lịch Khám Bệnh - Phần 2

--- server/src/models/specialty.js
+++ server/src/models/specialty.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       description: DataTypes.TEXT,
       image: DataTypes.STRING,
+      name: DataTypes.STRING,
     },
     {
       sequelize,
